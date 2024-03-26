98ad0540e62f59fab64c808ef9cce02a330c66c3
Juditmoon <juditgalo@outlook.fr>
2024-03-26 13:47:13 +0100

    first commit

---
+++ FrontEnd/assets/js/modal.js
@@ -0,0 +1,68 @@
+let modal = null
+const focusableSelector = "button, input, a, textarea"
+let focusables = []
+let previouslyFocusedElement = null
+
+const openModal = function(e) {
+    e.preventDefault()
+    modal = document.querySelector(e.target.getAttribute('href'))
+    focusables = Array.from(modal.querySelectorAll(focusableSelector))
+    previouslyFocusedElement = document.querySelector(':focus')
+    focusables[0].focus()
+    modal.style.display = null
+    modal.removeAttribute ('aria-hidden')
+    modal.setAttribute('aria-modal', 'true')
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.js-close-modal').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+}
+
+const closeModal = function (e) {
+    if (modal === null) return
+    if (previouslyFocusedElement !== null) previouslyFocusedElement.focus()
+    e.preventDefault()
+    window.setTimeout(function () {
+        modal.style.display = 'none'
+        modal = null   
+    }, 500)
+    modal.setAttribute ('aria-hidden','true')
+    modal.removeAttribute('aria-modal')
+    modal.removeEventListener('click', closeModal)
+    modal = null
+    modal.querySelector('.js-close-modal').removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+const focusInModal = function (e) {
+    e.preventDefault()
+    let index = focusables.findIndex(f => f === modal.querySelector(':focus'))
+    if (e.shiftKey === true) {
+        index--
+    } else {
+        index++
+    } 
+    if (index >= focusables.length) {
+        index = 0
+    }
+    if (index < 0) {
+        index = focusables.length - 1 
+    }
+    focusables [index].focus()
+}
+
+document.querySelectorAll(".js-modal").forEach(a =>{
+    a.addEventListener("click", openModal)
+})
+
+window.addEventListener('keydown', function(e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e)
+    }
+    if (e.key === "Tab" & modal !==null){
+        focusInModal(e)
+    }
+})
---
+++ FrontEnd/assets/js/works.js
@@ -0,0 +1,103 @@
+//Gestion des appel à l'API
+
+//Récupération des données des travaux 
+const reponse = await fetch('http://localhost:5678/api/works');
+const projets = await reponse.json()
+
+function genererProjets(projets) {
+    for (let i = 0; i < projets.length; i++) {
+
+        const article = projets[i];
+        //sélection de la div qui va contenir les travaux  
+        const sectionGalerie = document.querySelector(".galerie");
+        //on crée les éléments
+        const articleElement = document.createElement("article");
+        const imageElement = document.createElement("img");
+        const titreElement = document.createElement("p");
+         // On récupère les données importantes pour afficher les travaux
+        imageElement.src = article.imageUrl;
+        imageElement.setAttribute('categorie', article.categoryId);
+        titreElement.innerText = article.title;
+
+        // On relie les éléments img et title à leur parent card et l'article à la div qui contient la galerie 
+        sectionGalerie.appendChild(articleElement);
+        articleElement.appendChild(imageElement);
+        articleElement.appendChild(titreElement);
+
+    }
+
+}
+
+genererProjets(projets);
+
+
+ // Récupération des catégories 
+ async function genererFiltres() {
+    await fetch("http://localhost:5678/api/categories")
+    .then(response => response.json())
+    .then(dataCategories => {
+        // Test de récupération des catégories
+        //    console.table(dataCategories);
+        
+        // Sélection de la div qui va contenir les filtres
+        const filtres = document.querySelector(".filtres")
+        // Création du filtre Tous 
+        const tousFiltre = document.createElement('p');
+        tousFiltre.textContent = 'Tous';
+        tousFiltre.classList.add("filtreButton");
+        tousFiltre.classList.add("filtreActif")
+        filtres.appendChild(tousFiltre);
+        // Utilisation d'une boucle pour créer les noms des catégories
+        dataCategories.forEach((categorie) => {
+           const nameFiltres = document.createElement("p");
+           nameFiltres.innerText = categorie.name;
+           nameFiltres.id = categorie.id;
+           nameFiltres.classList.add("filtreButton");
+           filtres.appendChild(nameFiltres);   
+        });
+        //On fait l'événement click sur les filtres 
+        filtres.querySelectorAll('p').forEach((filtre) => {
+            filtre.addEventListener("click", function() {
+                // Sélection de l'id appliqué lors de la création des images des travaux 
+                const id = this.id;
+                // console.log(this.id)
+                document.querySelectorAll('.galerie img').forEach((image) => {
+                    // Si image = catégorie correspondante, afficher l'image
+                    if (image.getAttribute('categorie') === id) {
+                        image.parentElement.style.display = 'block';
+                    } else {
+                        // Sinon, masquer
+                        image.parentElement.style.display = 'none';
+                    }})
+                });
+            
+        });
+        // Pour le filtre Tous - réinitialisation de l'affichage 
+        tousFiltre.addEventListener('click', function () {
+            // Sélectionne toutes les images de la galerie
+            document.querySelectorAll('.galerie img').forEach(image => {
+            // Permet d'afficher tous les travaux 
+            image.parentElement.style.display = 'block';
+            });
+        }); 
+        //Sélection du filtre actif
+        const elements = filtres.querySelectorAll('p');
+        elements.forEach((element) => {
+            element.addEventListener("click", () => {
+                elements.forEach((element) => {
+                    // Au clic sur un bouton on retire la classe "active" du filtre "Tous"
+                    element.classList.remove("filtreActif");
+                    });
+                    // Et on ajoute cette classe active sur le bouton cliqué
+                    element.classList.add("filtreActif"); 
+            });
+                });  
+    })
+    
+ }
+
+genererFiltres()
+ 
+        
+
+
