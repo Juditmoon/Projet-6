8be57a415e1d2e750feafee7d648406a4bdc1327
Juditmoon <145652363+Juditmoon@users.noreply.github.com>
2024-04-02 21:33:51 +0200

    Update modal.js

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -14,91 +14,27 @@ function genererProjets(projets) {
         const articleElement = document.createElement("article");
         const imageElement = document.createElement("img");
         const titreElement = document.createElement("p");
+        const iconElement = document.createElement("i");
+
          // On récupère les données importantes pour afficher les travaux
         imageElement.src = article.imageUrl;
         imageElement.setAttribute('categorie', article.categoryId);
         imageElement.classList.add('image-size')
+        iconElement.classList.add("fa-trash-can")
+        iconElement.classList.add("fa-solid")
         sectionGalerie.classList.add('row')
 
         // On relie les éléments img et title à leur parent card et l'article à la div qui contient la galerie 
         sectionGalerie.appendChild(articleElement);
         articleElement.appendChild(imageElement);
         articleElement.appendChild(titreElement);
+        articleElement.appendChild(iconElement);
 
     }
 
 }
 
 genererProjets(projets);
-
-
- // Récupération des catégories 
- async function genererFiltres() {
-    await fetch("http://localhost:5678/api/categories")
-    .then(response => response.json())
-    .then(dataCategories => {
-        // Test de récupération des catégories
-        //    console.table(dataCategories);
-        
-        // Sélection de la div qui va contenir les filtres
-        const filtres = document.querySelector(".filtres")
-        // Création du filtre Tous 
-        const tousFiltre = document.createElement('p');
-        tousFiltre.textContent = 'Tous';
-        tousFiltre.classList.add("filtreButton");
-        tousFiltre.classList.add("filtreActif")
-        filtres.appendChild(tousFiltre);
-        // Utilisation d'une boucle pour créer les noms des catégories
-        dataCategories.forEach((categorie) => {
-           const nameFiltres = document.createElement("p");
-           nameFiltres.innerText = categorie.name;
-           nameFiltres.id = categorie.id;
-           nameFiltres.classList.add("filtreButton");
-           filtres.appendChild(nameFiltres);   
-        });
-        //On fait l'événement click sur les filtres 
-        filtres.querySelectorAll('p').forEach((filtre) => {
-            filtre.addEventListener("click", function() {
-                // Sélection de l'id appliqué lors de la création des images des travaux 
-                const id = this.id;
-                // console.log(this.id)
-                document.querySelectorAll('.galerie img').forEach((image) => {
-                    // Si image = catégorie correspondante, afficher l'image
-                    if (image.getAttribute('categorie') === id) {
-                        image.parentElement.style.display = 'block';
-                    } else {
-                        // Sinon, masquer
-                        image.parentElement.style.display = 'none';
-                    }})
-                });
-            
-        });
-        // Pour le filtre Tous - réinitialisation de l'affichage 
-        tousFiltre.addEventListener('click', function () {
-            // Sélectionne toutes les images de la galerie
-            document.querySelectorAll('.galerie img').forEach(image => {
-            // Permet d'afficher tous les travaux 
-            image.parentElement.style.display = 'block';
-            });
-        }); 
-        //Sélection du filtre actif
-        const elements = filtres.querySelectorAll('p');
-        elements.forEach((element) => {
-            element.addEventListener("click", () => {
-                elements.forEach((element) => {
-                    // Au clic sur un bouton on retire la classe "active" du filtre "Tous"
-                    element.classList.remove("filtreActif");
-                    });
-                    // Et on ajoute cette classe active sur le bouton cliqué
-                    element.classList.add("filtreActif"); 
-            });
-                });  
-    })
-    
- }
-
-genererFiltres()
- 
 
 
 // // get modal 
@@ -127,6 +63,24 @@ window.onclick = function(event){
     }
 }
 
+
+//on trash click, delete work photo
+const iconElement = document.querySelector('.fa-trash-can')
+ iconElement.addEventListener('click', function(){
+    const supprimer =  fetch (`http://localhost:5678/api/works/${idProject}`, {
+                method: 'DELETE',
+                headers: {
+                    'accept': '*/*',
+                    'Authorization': `Bearer ${monToken}`,
+                }
+      
+     })}
+    
+ )
+ 
+ 
+
+ 
 
         
 
