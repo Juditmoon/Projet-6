856018c600001557b2c73c4a67320862e3b4c3ac
Juditmoon <145652363+Juditmoon@users.noreply.github.com>
2024-04-10 16:33:42 +0200

    Update modal.js

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -176,8 +176,28 @@ modal2.addEventListener('click', function(event){
 // const ajouterPhoto = document.querySelector(".ajouterPhoto")
 // ajouterPhoto.addEventListener('change', (event) => {
 //   const fileList = event.target.files 
- 
 // })
+
+// var inputs = document.querySelectorAll( '.inputfile' );
+// Array.prototype.forEach.call( inputs, function( input )
+// {
+// 	var label	 = input.nextElementSibling,
+// 		labelVal = label.innerHTML;
+
+// 	input.addEventListener( 'change', function( e )
+// 	{
+// 		var fileName = '';
+// 		if( this.files && this.files.length > 1 )
+// 			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+// 		else
+// 			fileName = e.target.value.split( '\' ).pop()
+
+// 		if( fileName )
+// 			label.querySelector( 'span' ).innerHTML = fileName;
+// 		else
+// 			label.innerHTML = labelVal;
+// 	});
+// });
 
 
 
