61cbea9fd848743e0ff36e1bf701188f16d7a4a4
Juditmoon <145652363+Juditmoon@users.noreply.github.com>
2024-04-30 10:17:25 +0200

    Update modal.js

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -45,29 +45,31 @@ function genererProjetsModal(projetsModal) {
     const articleElement = document.createElement("article");
     const imageElement = document.createElement("img");
     const titreElement = document.createElement("p");
-    const iconElement = document.createElement("i");
+    const deleteButton = document.createElement("button");
 
     // On récupère les données importantes pour afficher les travaux
     imageElement.src = article.imageUrl;
     imageElement.setAttribute('categorie', article.categoryId);
     imageElement.classList.add('image-size')
-    iconElement.classList.add("fa-trash-can")
-    iconElement.addEventListener("click", (event) => {
+    deleteButton.classList.add('fa-trash-can')
 
+    deleteButton.addEventListener("click", (event) => {
       event.preventDefault();
+      event.stopPropagation()
+
 
       deleteWork(article.id, event);
-
     });
-    iconElement.classList.add("fa-solid")
-    iconElement.setAttribute("id", "work.id");
+
+    deleteButton.classList.add("fa-solid")
+    deleteButton.setAttribute("id", "workId");
     sectionGalerie.classList.add('row')
 
     // On relie les éléments img et title à leur parent card et l'article à la div qui contient la galerie 
     sectionGalerie.appendChild(articleElement);
     articleElement.appendChild(imageElement);
     articleElement.appendChild(titreElement);
-    articleElement.appendChild(iconElement);
+    articleElement.appendChild(deleteButton);
 
   }
 }
@@ -80,7 +82,7 @@ const modal = document.getElementById('myModal')
 // // get the button that opens the modal
 const btn = document.getElementById('myBtn')
 
-// // get the <span> element that closes the modal
+// // get the <span> element that closes the modal(c'est le icon croix)
 const span = document.querySelector('.close')
 
 // // when the user clicks the button, open the modal
@@ -88,23 +90,23 @@ btn.onclick = function () {
   modal.style.display = "block"
 }
 
-// when the user clicks on <span>, close the modal 
+// when the user clicks on <span>(the cross), close the modal
 span.onclick = function () {
   modal.style.display = "none"
 }
 
-// when the user clicks anywhere outside of the modal, close it 
+// // when the user clicks anywhere outside of the modal, close it 
 window.addEventListener("click", function (event) {
-  if (event.target == modal) {
+  if (event.target == modal ) {
     modal.style.display = "none"
   }
 }
 )
 
+// Gestion de supprimer de travaux 
 const token = localStorage.getItem('token');
 
-
-async function deleteWork(workId, event) {
+async function deleteWork(workId) {
 
   const response = await fetch(`http://localhost:5678/api/works/${workId}`, {
     method: "DELETE",
@@ -138,27 +140,17 @@ arrow.addEventListener("click", function () {
   modal2.style.display = "none"
 })
 
+
 //gére le click sur la croix
 const closeIt = document.querySelector(".close2")
 closeIt.addEventListener("click", function () {
+
   const modal1 = document.getElementById('myModal')
   modal1.style.display = "none"
   const modal2 = document.getElementById('modalAjout')
   modal2.style.display = "none"
 })
 
-//fermer la modale onclick dehors de la modal
-function closeModal2() {
-  const modal2 = document.getElementById('modalAjout')
-  modal2.addEventListener('click', function (event) {
-    // console.log(event.target)
-    if (event.target == modal2) {
-      modal2.style.display = "none"
-    }
-  })
-
-}
-closeModal2()
 
 //click ajouter photo, direction mon PC
 const fileInput = document.querySelector('input')
@@ -183,6 +175,24 @@ fileInput.addEventListener('change', () => {
   })
 })
 
+//fermer la modale onclick dehors de la modal
+function closeModal2() {
+  const form = document.getElementById('form')
+  const modal2 = document.getElementById('modalAjout')
+  const previewImage = document.getElementById('previewImage');
+  modal2.addEventListener('click', function (event) {
+    
+    // console.log(event.target)
+    if (event.target == modal2) {
+      modal2.style.display = "none"
+      form.reset()
+      previewImage.src = '';
+    }
+  })
+
+}
+closeModal2()
+
 
 function getTitreValue() {
   var titreValue = document.getElementById('titre').value;
@@ -200,9 +210,9 @@ function changeButtonColor() {
   var validerButton = document.getElementById('validerButton');
 
   if (fieldInput !== '' && titreInput !== '' && optionValue !== 'emptyOption') {
-      validerButton.style.backgroundColor = '#1D6154';
+    validerButton.style.backgroundColor = '#1D6154';
   } else {
-      validerButton.style.backgroundColor = '#A7A7A7';
+    validerButton.style.backgroundColor = '#A7A7A7';
   }
 }
 
@@ -212,8 +222,8 @@ document.getElementById('titre').addEventListener('input', changeButtonColor);
 document.getElementById('list').addEventListener('change', changeButtonColor);
 
 let validerButton = document.getElementById('validerButton');
-validerButton.addEventListener('click', function(){
- 
+validerButton.addEventListener('click', function () {
+
   var fieldInput = document.getElementById('file').value;
   console.log(fieldInput)
   var titreInput = document.getElementById('titre').value;
@@ -221,30 +231,28 @@ validerButton.addEventListener('click', function(){
   var validerButton = document.getElementById('validerButton');
 
   if (fieldInput !== '' && titreInput !== '' && optionValue !== 'emptyOption') {
-      validerButton.style.backgroundColor = '#1D6154';
+    validerButton.style.backgroundColor = '#1D6154';
   } else {
-      alert('Veuillez remplir tous les champs')  
+    alert('Veuillez remplir tous les champs')
   }
 });
 
 let alertTriggered = false;
 
 function stopFormSubmission(event) {
-    if (alertTriggered) {
-        event.preventDefault(); // Prevent the form from submitting
-    }
+  if (alertTriggered) {
+    event.preventDefault(); // Prevent the form from submitting
+  }
 }
 
 // Example of how to trigger the alert
 function triggerAlert() {
-    // alert('Veuillez remplir tous les champs');
-    alertTriggered = true; // Set the flag to true when the alert is triggered
+  // alert('Veuillez remplir tous les champs');
+  alertTriggered = true; // Set the flag to true when the alert is triggered
 }
 
 // Add an event listener to the form submit event
 document.getElementById('form').addEventListener('submit', stopFormSubmission);
-
-
 function stopModalClose(event) {
   if (alertTriggered) {
     event.preventDefault();
@@ -258,98 +266,52 @@ function triggerAlertAndPreventModalClose() {
 }
 document.getElementById('modalAjout').addEventListener('hide.bs.modal', stopModalClose);
 document.getElementById('validerButton').addEventListener('click', () => {
-  triggerAlertAndPreventModalClose, addWork});
+  triggerAlertAndPreventModalClose, addWork
+});
 
 
-
-// // INDEX : 5-/ GESTION AJOUT D'UN PROJET        ///
-// ////////////////////////////////////////////////////
-
+// // Ajouter un projet
 const btnAjouterProjet = document.querySelector("#validerButton");
 btnAjouterProjet.addEventListener("click", addWork);
 
-// // Ajouter un projet
-function addWork() {
+async function addWork(event) {
+  event.preventDefault()
   const titre = document.querySelector("#titre").value;
   const categorie = document.querySelector("#list").value;
-  const file= document.querySelector('#file');
-    try {
-      const formData = new FormData();
-      formData.append('title', titre);
-      formData.append('category', categorie);
-      formData.append('image', file.files[0]);
+  const file = document.querySelector('#file');
+  try {
+    const formData = new FormData();
+    formData.append('title', titre);
+    formData.append('category', categorie);
+    formData.append('image', file.files[0]);
 
-        const response = fetch("http://localhost:5678/api/works", {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-            body: formData,
-        });
+    const response = await fetch("http://localhost:5678/api/works", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
 
-        if (response.status === 201) {
-            alert("Projet ajouté avec succès :)");
-            modaleProjets(dataAdmin);
-            backToModale();
-            generationProjets(data, null);
-            
-        }  else if (response.status === 500) {
-            alert("Erreur serveur");
-        } else if (response.status === 401) {
-            alert("Vous n'êtes pas autorisé à ajouter un projet");
-           
-    }}
+    if (response.status === 201) {
+      alert("Projet ajouté avec succès :)");
+    } else if (response.status === 500) {
+      alert("Erreur serveur");
+    } else if (response.status === 401) {
+      alert("Vous n'êtes pas autorisé à ajouter un projet");
 
-    catch (error) {
-        console.log(error);
-}}
-// addWork()
+    }
+  }
+
+  catch (error) {
+    console.log(error);
+  }
+}
 
 
 
 
 
-
-// // After a successful file upload, save the file reference to localStorage
-// function saveFileReference(fieldInput) {
-//   // Ensure that a file has been uploaded
-//   if (fieldInput.files && fieldInput.files[0]) {
-//     // Create a URL for the uploaded file
-//     const imageUrl = URL.createObjectURL(fieldInput.files[0]);
-    
-//     // Retrieve the existing gallery array from localStorage, or create a new one if it doesn't exist
-//     const gallery = JSON.parse(localStorage.getItem('gallery')) || [];
-//     gallery.push(imageUrl);
-
-//     // Save the updated gallery array back to localStorage
-//     localStorage.setItem('gallery', JSON.stringify(gallery));
-//   }
-// }
-
-// // let fieldInput = document.getElementById('file');
-// // Call saveFileReference after a successful file upload, passing the file input element as an argument
-// fieldInput.addEventListener('change', function() {
-//   saveFileReference(this);
-// });
-
-// // Function to display the gallery images
-// function displayGallery() {
-//   const galleryContainer = document.querySelector('galerie');
-//   const gallery = JSON.parse(localStorage.getItem('gallery')) || [];
-
-//   // Clear the gallery container
-//   galleryContainer.innerHTML = '';
-
-//   // Create an img element for each file reference and append it to the container
-//   gallery.forEach(imageUrl => {
-//     const img = document.createElement('img');
-//     img.src = imageUrl;
-//     galleryContainer.appendChild(img);
-//   });
-// }
-
-// // Call displayGallery on page load to show the images
-// window.addEventListener('load', displayGallery);
 
 
 
